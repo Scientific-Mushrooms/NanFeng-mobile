796da68df13bc5f8c526d7ee22af5c3aa145af2f
Winning12 <wenhao_jun@outlook.com>
2018-07-24 15:03:05 +0800

    test gridview

---
+++ src/components/GridListItem.js
@@ -0,0 +1,29 @@
+import React, { PureComponent } from 'react'
+import { StyleSheet, View, Alert, TouchableOpacity, Image, TouchableHighlight, Dimensions } from 'react-native'
+import { Button, ListItem, Left, Right, Body, Thumbnail, Text, Icon } from 'native-base'
+
+
+const { width, height } = Dimensions.get('window')
+export default class GridListItem extends PureComponent {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const rowID = this.props.index
+    const rowData = this.props.item
+    return (
+      <TouchableOpacity onPress={() => this.props.onPress('GridView', rowID, rowData)}>
+        <View style={{ margin: 0.5, width: width / 2, paddingBottom: 15 }}>
+          <Text >ID: {rowID}</Text>
+          <Text>{rowData}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+}
+
+const styles={
+    gridText:{
+    }
+}
--- src/pages/tabPages/school.js
+++ src/pages/tabPages/school.js
@@ -3,36 +3,86 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  Alert,
+  TouchableOpacity, 
+  Dimensions
 } from 'react-native';
 import { connect } from 'react-redux';
-import { increase, decrease, reset } from '../../redux/action';
+import {UltimateListView} from "react-native-ultimate-listview";
 
+const { width, height } = Dimensions.get('window')
 class School extends Component {
-  _onPressReset() {
-    this.props.dispatch(reset());
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      layout: 'grid',
+      data:[]
+    }
   }
 
-  _onPressInc() {
-    this.props.dispatch(increase());
-  }
+  sleep = (time) => new Promise(resolve => setTimeout(() => resolve(), time));
 
-  _onPressDec() {
-    this.props.dispatch(decrease());
-  }
+  onFetch = async(page = 1, startFetch, abortFetch) => {
+    fetch('http://118.25.56.186/data', {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json'
+        }
+        }).then((response) => response.json())
+        .then((response) => {
+          let pageLimit = 40;
+          var json = response;
+          this.setState({
+            data: json,
+          });
+          let rowData =this.state.data
+          startFetch(rowData, pageLimit);
+        })
+        .catch((error) => {
+          if (error) {
+            abortFetch();
+            console.log('error', error);
+          }
+      });   
+  };
+
+  renderItem = (item, index, separator) => {
+    return(
+    <TouchableOpacity>
+        <View style={{ margin: 0.5, width: width / 2, paddingBottom: 15 }}>
+          <Text >ID: {index}</Text>
+          <Text>{item.author.name}</Text>
+        </View>
+    </TouchableOpacity>
+    );
+  };
+
+  onPress = (index, item) => {
+      Alert.alert(index, `You're pressing on ${item}`);
+  };
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.counter}>school</Text>
-      </View>
+      <UltimateListView
+          ref={(ref) => this.listView = ref}             
+          onFetch={this.onFetch} 
+          refreshableMode="basic" //basic or advanced
+          item={this.renderItem}  //this takes two params (item, index)
+          numColumns={2} //to use grid layout, simply set gridColumn > 1
+
+             //----Extra Config----
+             //header={this.renderHeaderView}
+             //paginationFetchingView={this.renderPaginationFetchingView}           
+             //paginationFetchingView={this.renderPaginationFetchingView}
+             //paginationAllLoadedView={this.renderPaginationAllLoadedView}
+             //paginationWaitingView={this.renderPaginationWaitingView}
+             //emptyView={this.renderEmptyView}
+             //separator={this.renderSeparatorView}
+           />
     );
   }
 }
-
-const mapStateToProps = state => ({
-    counter: state.counter
-})
 
 const styles = StyleSheet.create({
     container: {
@@ -53,4 +103,4 @@ const styles = StyleSheet.create({
     },
   });
 
-export default connect(mapStateToProps)(School);
+export default School;
