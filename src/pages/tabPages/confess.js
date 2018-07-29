aa53e63beea01d536e1b92aabb7b0a79b386b44d
NewbieMiao <newblush.miao@gmail.com>
2018-07-29 16:33:59 +0800

    ConfessHead with navigator

--- src/pages/tabPages/confess.js
+++ src/pages/tabPages/confess.js
@@ -1,158 +1,52 @@
 import React, { Component } from 'react';
 import {
+    AppRegistry,
     StyleSheet,
     Text,
     View,
-    TouchableOpacity,
-    Image,
+    TouchableOpacity
 } from 'react-native';
-import Modal from 'react-native-modalbox';
-import Button from 'react-native-button';
-import { connect } from 'react-redux';
-import { setFirst } from '../../redux/action';
-import { StackNavigator } from 'react-navigation';
-import Page1 from './confessPages/test1';
-
-
-class ConfessHome extends Component {
-
-    static navigationOptions = {
-        title: 'ConfessHome',
-    };
-
-    constructor() {
-        super();
-        this.state = {
-            isOpen: false,
-            isDisabled: false,
-            swipeToClose: true,
-            sliderValue: 0.3
-        };
-    }//mb初始设置
-
-    ifFirstReset() {
-        this.props.dispatch(setFirst());
+import {
+    StackNavigator,
+} from 'react-navigation';
+import ConfessHome from './confessPages/ConfessHome';
+import Page1 from './confessPages/Page1';
+import {connect} from "react-redux";
+import {notFirst} from "../../redux/action";
+// StackNavigator配置，默认显示MianVC页面
+const ConfessNavigator = StackNavigator(
+    {
+        ConfessHome: {screen: ConfessHome },
+        Page1: {screen: Page1},
+    },
+    {
+        initialRouteName: 'ConfessHome',//默认路由，就是第一个要显示的页面
     }
-
-
-
-    render() {
-        const { navigate } = this.props.navigation;
-        return (
-
-            <View style={{flex: 1, backgroundColor: 'rgb(240,240,240)'}}>
-
-                <View style={styles.header}>
-
-                    <View style={styles.center}>
-                        <Text style={{color: '#585858',fontSize:20}}
-                              onPress={() => this.refs.modalBox.open()}
-                        >全部动态</Text>
-                    </View>
-
-                </View>
-
-                <View style={styles.container}>
-                    <Modal style={styles.modalBox}
-                           backdrop={true} //背景默认黑色50%透明度
-                           position={"top"}  //悬停位置
-                           ref={"modalBox"}
-                           entry={"top"} //从上端进入
-                           animationDuration={400}//动画速度
-                    >
-                        <TouchableOpacity
-                            style={styles.button}
-                            onPress={() => navigate('Page1')}>
-                        <Text style={[styles.text, {color: "black"}]}>Page1</Text>
-                        </TouchableOpacity>
-                        <Text style={[styles.text, {color: "black"}]}>Page2</Text>
-                    </Modal>
-
-                    <Text style={styles.counter}>confess</Text>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={this.ifFirstReset.bind(this)}>
-                        <Text style={styles.btText}>点击重置redux状态</Text>
-                        <Text style={styles.btText}>以测试redux-persist</Text>
-                    </TouchableOpacity>
-                </View>
-
-            </View>
-
-        );
-    }
-}
-
-
-const mapStateToProps = state => ({
-    counter: state.counter
-})
+);
 
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-
-    //mb用样式
-    btn: {
-        margin: 10,
-        backgroundColor: "#3B5998",
-        color: "white",
-        padding: 10
-    },
-    modalBox: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 230,
-        backgroundColor: 'rgb(240,240,240)'
-    },
-    text: {
-        color: "black",
-        marginTop: 30,
-        marginBottom: 30,
-        fontSize: 22
-    },
-
-    //头部
-    header: {
-        flexDirection: 'row',
-        height: 50,
-        borderBottomWidth:2,
-        borderColor:'rgb(230,230,230)',
-        backgroundColor:'rgb(248,248,248)',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    center: {
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
     },
 });
 
-const Navigator=StackNavigator(
-    {
-        Home:{screen:ConfessHome},
-        Page1:{screen:Page1},
-        //Page2:{screen:test2},
+class CN extends Component {
+
+    static navigationOptions = {
+        header: null,
+    };
+
+
+    render() {
+        return (
+            <ConfessNavigator />
+        );
     }
-);
+}
+const mapStateToProps = state => ({
+    ifFirst: state.ifFirst
+})
 
-
-
-
-export default connect(mapStateToProps)(ConfessHome);
+export default connect(mapStateToProps)(CN);
