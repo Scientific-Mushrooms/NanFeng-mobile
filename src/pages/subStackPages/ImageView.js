a8a459c7f750c23cc47515694c6faf4b53b746c9
Winning12 <wenhao_jun@outlook.com>
2018-08-02 15:37:59 +0800

    update imageview

--- src/pages/subStackPages/ImageView.js
+++ src/pages/subStackPages/ImageView.js
@@ -6,6 +6,8 @@ import {
   StyleSheet,
   Text,
   View,
+  Alert,
+  TouchableOpacity
 } from 'react-native';
 import Image from '../../components/TransformableImage';
 
@@ -13,14 +15,42 @@ class ImageView extends Component {
   constructor(props) {
     super(props);
   }
+  
+  static navigationOptions = {
+    header: null,
+  };
 
   render() {
     return(
-    <Image
-          style={{width: Dimensions.get('window').width,height:Dimensions.get('window').height}}
-          source={this.props.navigation.state.params.imgsrc}
-        />)
+    <View style={{flex:1,backgroundColor:'black'}}>
+        <TouchableOpacity style={{flex:1}}>
+            <Image 
+            source={require('../../assets/cancel.png')} 
+            style={styles.icon} 
+            resizeMode='contain'/>
+        </TouchableOpacity>
+        <Image
+            onTransformGestureReleased={(object)=>this.handleScale}
+            style={{width: Dimensions.get('window').width,height:Dimensions.get('window').height*19/20}}
+            source={this.props.navigation.state.params.imgsrc}
+        />
+    </View>)
   }
+
+  handleScale=(object)=>{
+      Alsert.alert(object.scale)
+      if(object.scale<1){
+          return false
+      }else
+          return true
+  }
+
 }
 
+const styles={
+  icon:{
+    margin:5,
+    width:Dimensions.get('window').width/20
+  },
+  }
 export default ImageView;
